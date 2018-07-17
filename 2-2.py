4a757626a0cbeca39a18b2ac036d062e635fc056
stephen2232 <41285202+stephen2232@users.noreply.github.com>
2018-07-17 17:15:36 +0800

    fetch urls

---
+++ 2-2.py
@@ -0,0 +1,8 @@
+import urllib.request
+page=urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty/html")
+text=page.read().decode("utf8")
+price=text.find("$")
+start=price+1
+end=price+5
+totalprice=text[start:end]
+print(totalprice)
---
+++ 2-3.py
@@ -0,0 +1,10 @@
+import urllib.request
+page=urllib.request.urlopen("http://beans-r-us.appspot.com/prices-loyalty/html")
+text=page.read().decode("utf8")
+price=text.find("$")
+start=price+1
+end=price+5
+totalprice=float(text[start:end])
+while totalprice <= 4.74:
+    print(totalprice)
+print(totalprice)
